0f2af92b41478a6b20bfcfe2b2062c62b351f20f
Saiful <saifulsaif321@gmail.com>
2022-11-15 13:25:34 +0600

    implementation add #error occured

--- app/src/main/java/com/saiful/androidlibrary/MainActivity.kt
+++ app/src/main/java/com/saiful/androidlibrary/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.saiful.customlist.ListActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

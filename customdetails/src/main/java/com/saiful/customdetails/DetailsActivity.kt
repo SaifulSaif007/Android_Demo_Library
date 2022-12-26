1e0f0e59c17b3f42f4bf0832ebb9db4c69cd69d6
Saiful <saifulsaif321@gmail.com>
2022-12-26 12:51:54 +0600

    build version

--- customdetails/src/main/java/com/saiful/customdetails/DetailsActivity.kt
+++ customdetails/src/main/java/com/saiful/customdetails/DetailsActivity.kt
@@ -2,10 +2,13 @@ package com.saiful.customdetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+
     }
 }
--- customlist/src/main/java/com/saiful/customlist/ListActivity.kt
+++ customlist/src/main/java/com/saiful/customlist/ListActivity.kt
@@ -1,15 +1,21 @@
 package com.saiful.customlist
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.saiful.customdetails.DetailsActivity
 
 class ListActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        val textview = findViewById<TextView>(R.id.list_text)
+        textview.text = BuildConfig.FLAVOR + BuildConfig.BUILD_TYPE
 
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
